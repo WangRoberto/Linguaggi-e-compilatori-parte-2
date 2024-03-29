121081883c54ab17eb5d3b4eb85df255e7f39667
Jun Wu Wang <wng.mattia@gmail.com>
2024-03-29 17:35:04 +0100

    Erase instructions after replacing their uses

--- jun-wu-wang/LocalOpts.cpp
+++ jun-wu-wang/LocalOpts.cpp
@@ -9,65 +9,71 @@
 #include "llvm/Transforms/Utils/LocalOpts.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/InstrTypes.h"
+#include <vector>
 
 using namespace llvm;
 
-bool runOnInstruction(Instruction &I) {
-  // Check if the instruction is a BinaryOperator
-  BinaryOperator *BinaryI = dyn_cast<BinaryOperator>(&I);
-  if (!BinaryI) {
-    return false;
-  }
-
-  // Check if the instruction is a mul
-  if (BinaryI->getOpcode() != Instruction::Mul) {
-    return false;
-  }
-  outs() << *BinaryI << ":\n";
-  
-  // Check if there is and eventually which operand is an immediate
-  ConstantInt *Immediate = dyn_cast<ConstantInt>(BinaryI->getOperand(0));
-  Value *Val = BinaryI->getOperand(1);
-  if (!Immediate) {
-    Immediate = dyn_cast<ConstantInt>(BinaryI->getOperand(1));
-    Val = BinaryI->getOperand(0);
-    if (!Immediate) {
-      outs() << "\thas no immediate\n";
-      return false;
-    }
-  }
-
-  // Check if immediate is a power of 2
-  APInt ImmediateValue = Immediate->getValue();
-  if (!ImmediateValue.isPowerOf2()) {
-    outs() << "\thas no immediate that is power of 2\n";
-    return false;
-  }
-
-  // Create a shl instruction
-  int32_t N = ImmediateValue.exactLogBase2();
-  LLVMContext &context = I.getContext(); 
-  IntegerType *Int32Type = IntegerType::get(context, 32);
-  ConstantInt *Shifts = ConstantInt::get(Int32Type, N);
-
-  Instruction *NewInst = BinaryOperator::Create(
-  BinaryOperator::Shl, Val, Shifts);
-
-  // Insert the new instruction
-  NewInst->insertAfter(&I);
-  I.replaceAllUsesWith(NewInst);
-  outs() << "\thas immediate that is power of 2\n";
-
-  return true;
-}
-
 bool runOnBasicBlock(BasicBlock &B) {
   bool Transformed = false;
+  std::vector<Instruction*> toErase;
 
   for (auto Iter = B.begin(); Iter != B.end(); ++Iter) {
-    if (runOnInstruction(*Iter)) {
-      Transformed = true;
+    Instruction &I = *Iter;
+    // Check if the instruction is a BinaryOperator
+    BinaryOperator *BinaryI = dyn_cast<BinaryOperator>(&I);
+    if (!BinaryI) {
+      continue;
     }
+  
+    // Check if the instruction is a mul
+    if (BinaryI->getOpcode() != Instruction::Mul) {
+      continue;
+    }
+    outs() << *BinaryI << ":\n";
+    
+    // Check if there is and eventually which operand is an immediate
+    ConstantInt *Immediate = dyn_cast<ConstantInt>(BinaryI->getOperand(0));
+    Value *Val = BinaryI->getOperand(1);
+    if (!Immediate) {
+      Immediate = dyn_cast<ConstantInt>(BinaryI->getOperand(1));
+      Val = BinaryI->getOperand(0);
+      if (!Immediate) {
+        outs() << "\thas no immediate\n";
+	continue;
+      }
+    }
+  
+    // Check if immediate is a power of 2
+    APInt ImmediateValue = Immediate->getValue();
+    if (!ImmediateValue.isPowerOf2()) {
+      outs() << "\thas no immediate that is power of 2\n";
+      continue;
+    }
+    outs() << "\thas immediate that is power of 2\n";
+  
+    // Create shl instruction
+    int32_t N = ImmediateValue.exactLogBase2();
+    LLVMContext &context = I.getContext(); 
+    IntegerType *Int32Type = IntegerType::get(context, 32);
+    ConstantInt *Shifts = ConstantInt::get(Int32Type, N);
+  
+    Instruction *NewInst = BinaryOperator::Create(
+    BinaryOperator::Shl, Val, Shifts);
+  
+    // Insert new instruction
+    NewInst->insertAfter(&I);
+    I.replaceAllUsesWith(NewInst);
+
+    // Add old instruction to vector of instructions to be erased
+    toErase.push_back(&I);
+
+    Transformed = true;
+  }
+
+  // Erase old instructions
+  for (auto it = toErase.rbegin(); it != toErase.rend(); ++it) {
+    Instruction &InstToErase = **it;
+    InstToErase.eraseFromParent();
   }
 
   return Transformed;
