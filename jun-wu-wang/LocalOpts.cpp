a0cbc21f02e2e2d3a49a1351757d246b8262b49a
Jun Wu Wang <wng.mattia@gmail.com>
2024-04-03 11:09:01 +0200

    Optimize strength reduction pass

--- jun-wu-wang/LocalOpts.cpp
+++ jun-wu-wang/LocalOpts.cpp
@@ -11,8 +11,87 @@
 #include "llvm/IR/InstrTypes.h"
 #include <vector>
 #include <functional>
+#include <cstring>
 
 using namespace llvm;
+
+bool optimizeUdiv(BinaryOperator &BinaryI) {
+  ConstantInt *Immediate = dyn_cast<ConstantInt>(BinaryI.getOperand(1));
+  Value *Val = BinaryI.getOperand(0);
+  if (!Immediate) {
+    return false;
+  }
+
+  APInt ImmediateValue = Immediate->getValue();
+  if (!ImmediateValue.isPowerOf2()) {
+    return false;
+  }
+
+  // Create shl instruction
+  int32_t N = ImmediateValue.exactLogBase2();
+  ConstantInt *Shifts = ConstantInt::get(Immediate->getType(), N);
+
+  Instruction *NewInst = BinaryOperator::Create(
+  BinaryOperator::LShr, Val, Shifts);
+  
+  // Insert new instruction
+  NewInst->insertAfter(&BinaryI);
+  BinaryI.replaceAllUsesWith(NewInst);
+
+  outs() << BinaryI << " has been replaced by a lshr instruction (strength reduction)\n";
+  return true;
+}
+
+bool optimizeMul(BinaryOperator &BinaryI) {
+  // Check if there are immediate operands powers of 2 or "almost" power of 2
+  int32_t diff = 1;
+  for (unsigned i = 0; i < 3; ++i, diff = (diff+1)%3) {
+    for (unsigned Iter = 0; Iter != BinaryI.getNumOperands(); ++Iter) {
+      ConstantInt *Immediate = dyn_cast<ConstantInt>(BinaryI.getOperand(Iter));
+      if (!Immediate) {
+        continue;
+      }
+  
+      // Check if it is power of 2 or "almost" power of 2
+      APInt ImmediateValue = Immediate->getValue();
+      ImmediateValue += (diff-1);
+      if (!ImmediateValue.isPowerOf2()) {
+        continue;
+      }
+  
+      Value* Val = BinaryI.getOperand((Iter+1)%(BinaryI.getNumOperands()));
+
+      // Create shl instruction
+      int32_t N = ImmediateValue.exactLogBase2();
+      ConstantInt *Shifts = ConstantInt::get(Immediate->getType(), N);
+  
+      Instruction *NewInst = BinaryOperator::Create(BinaryOperator::Shl, Val, Shifts);
+  
+      // Insert shl instruction
+      NewInst->insertAfter(&BinaryI);
+
+      // Insert eventual add or sub instruction
+      std::string str = "";
+      if ((diff-1) == 1) {
+        Instruction &prev = *NewInst;
+	NewInst = BinaryOperator::Create(BinaryOperator::Add, &prev, Val);
+	NewInst->insertAfter(&prev);
+	str = "and an add";
+      } else if ((diff-1) == -1) {
+        Instruction &prev = *NewInst;
+	NewInst = BinaryOperator::Create(BinaryOperator::Sub, &prev, Val);
+	NewInst->insertAfter(&prev);
+	str = "and a sub";
+      }
+      BinaryI.replaceAllUsesWith(NewInst);
+  
+      outs() << BinaryI << " has been replaced by a shl " << str << " instruction (strength reduction)\n";
+  
+      return true;
+    }
+  }
+  return false;
+}
 
 bool runOnBasicBlockStrengthReduction(BasicBlock &B) {
   bool Transformed = false;
@@ -25,45 +104,25 @@ bool runOnBasicBlockStrengthReduction(BasicBlock &B) {
     if (!BinaryI) {
       continue;
     }
-  
-    // Check if the instruction is a mul
+
+    // Check if the instruction in a mul or udiv
+    if (BinaryI->getOpcode() != Instruction::Mul && BinaryI->getOpcode() != Instruction::SDiv) {
+      continue;
+    }
+
+    // Optimize the instruction
+    bool optimized = false;
     if (BinaryI->getOpcode() != Instruction::Mul) {
-      continue;
+      optimized = optimizeUdiv(*BinaryI);
+    } else {
+      optimized = optimizeMul(*BinaryI);
     }
-    
-    // Check if there is and eventually which operand is an immediate
-    ConstantInt *Immediate = dyn_cast<ConstantInt>(BinaryI->getOperand(0));
-    Value *Val = BinaryI->getOperand(1);
-    if (!Immediate) {
-      Immediate = dyn_cast<ConstantInt>(BinaryI->getOperand(1));
-      Val = BinaryI->getOperand(0);
-      if (!Immediate) {
-	continue;
-      }
-    }
-  
-    // Check if immediate is a power of 2
-    APInt ImmediateValue = Immediate->getValue();
-    if (!ImmediateValue.isPowerOf2()) {
-      continue;
-    }
-  
-    // Create shl instruction
-    int32_t N = ImmediateValue.exactLogBase2();
-    ConstantInt *Shifts = ConstantInt::get(Immediate->getType(), N);
-  
-    Instruction *NewInst = BinaryOperator::Create(
-    BinaryOperator::Shl, Val, Shifts);
-    
-    // Insert new instruction
-    NewInst->insertAfter(&I);
-    I.replaceAllUsesWith(NewInst);
 
     // Add old instruction to vector of instructions to be erased
-    toErase.push_back(&I);
-
-    outs() << *BinaryI << " has been replaced by a shl instruction (strength reduction)\n";
-    Transformed = true;
+    if (optimized) {
+      toErase.push_back(&I);
+      Transformed = true;
+    }
   }
 
   // Erase old instructions
