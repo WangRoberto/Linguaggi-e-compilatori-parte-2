5baac73d7193f1a42aa31a14ac5335975e351bb7
Roberto Wang <rrobertowang@gmail.com>
2024-05-31 11:52:30 +0200

    Assignment 4 Fix 2

--- Quarto Assignment/LoopFusionPass.cpp
+++ Quarto Assignment/LoopFusionPass.cpp
@@ -17,8 +17,13 @@
 
 using namespace llvm;
 
+/*Stampa:
+1. Il numero di Loop
+2. Il PreHeader
+3. Blocchi del Loop*/
+
 void myPrintLoop(Loop & loop, int cont){
-  outs() << "\n -------- Loop n°" << cont << " -------- \n";
+  outs() << "\n ------------------------ Loop n°" << cont << " ------------------------ \n";
   outs() << "\n" << loop << "\n -------- PreHeader -------- \n";
   
   //outs() << loop << "\n";
@@ -28,67 +33,58 @@ void myPrintLoop(Loop & loop, int cont){
   for(auto B = loop.block_begin(); B != loop.block_end(); ++B){
     outs() << **B;
   }
+
+  outs() << "\n -------- Successore(Exit Block) -------- \n" << *loop.getExitBlock();
+
 }
 
-/*Bool*/
+/*Controlla se i Loop sono adiacenti:
+1. Confronta il successore del L0 con il PreHeader/Guardia di L1*/
 
-void checkLoopAdiacenti(BasicBlock * successoreLoop1, BasicBlock * BBloop2){
-  if(successoreLoop1 == BBloop2){
-    outs() << "\n -------- Loop Adiecenti -------- \n";
-    return;
+bool checkLoopAdiacenti(BasicBlock * successoreLoop0, BasicBlock * BBTopL1){
+  if(successoreLoop0 == BBTopL1){
+    return true;
   }
-  outs() << "\n -------- Loop non Adiacenti -------- \n";
+  return false;
 }
+
+/*Si ottiene il BasicBlock della Guardia oppure quello del PreHeader*/
 
 BasicBlock * topLoopBB(Loop & loop, BasicBlock * exitBlock){
   if(loop.isGuarded()){
-    outs() << "\n -------- Loop è Guarded --------- \n";
+    //outs() << "\n -------- Loop è Guarded --------- \n";
     return loop.getLoopGuardBranch()->getParent();
-    //BBTop = checkLoopAdiacenti(exitBlock, BBGuard);
   }
   
-  outs() << "\n -------- Loop Unguarded -------- \n";
-  //BBTop = checkLoopAdiacenti(exitBlock, BBPreHeader);
+  //outs() << "\n -------- Loop Unguarded -------- \n";
   return loop.getLoopPreheader();
-  
-  //return BBTop;
 }
 
-/*Bool + Elimina i FOR*/
+/*Controlla se L0 e L1 sono Control Flow Equivalenti:
+1. L0 Domina L1
+2. L1 PostDomina L0*/
 
-void checkLoopControlFlowEquivalenti(DominatorTree & DT, PostDominatorTree & PDT, BasicBlock * BBTopL1, BasicBlock * BBTopL0, BasicBlock * exitBlock, int cont){
+bool checkLoopControlFlowEquivalent(DominatorTree & DT, PostDominatorTree & PDT, BasicBlock * BBTopL1, BasicBlock * BBTopL0, BasicBlock * exitBlock, int cont){
   bool isDominated = false;
   bool isPostDominated = false;
 
   if(BBTopL0){
-    for(auto & I : *BBTopL0){
-      if(DT.dominates(&I, BBTopL1)){
-        isDominated = true;
-        break;
-      }
+    Instruction & I0 = *BBTopL0->begin();
+    if(DT.dominates(&I0, BBTopL1)){
+      isDominated = true;
     }
 
-    
-  
-    for(auto & I: *BBTopL1){
-        for(auto & I2: *BBTopL0){
-          if(PDT.dominates(&I, &I2)){
-            isPostDominated = true;
-            break;
-          }
-        }
+    Instruction & I1 = *BBTopL1->begin();
 
+    if(PDT.dominates(&I1, &I0)){
+      isPostDominated = true;
     }
 
     outs() << "\n L0 domina L1? " << (isDominated? "True\n" : "False\n");
     outs() << "\n L1 postdomina L0? " << (isPostDominated? "True\n" : "False\n");
-
-    if(isDominated && isPostDominated){
-      outs() << "\n -------- Loop n°" << (cont - 1) << " e Loop n°" << cont << " sono control flow equivalenti -------- \n";
-    }
   }
 
-  
+  return isDominated & isPostDominated;
 }
 
 PreservedAnalyses LoopFusionPass::run(Function &F, FunctionAnalysisManager &AM) {
@@ -116,26 +112,33 @@ PreservedAnalyses LoopFusionPass::run(Function &F, FunctionAnalysisManager &AM) 
     exitBlock*/
 
     BasicBlock * BBTopL1 = topLoopBB(loop, exitBlock);
-    checkLoopAdiacenti(exitBlock, BBTopL1);
+    bool isLoopAdiacenti = checkLoopAdiacenti(exitBlock, BBTopL1);
+    if(isLoopAdiacenti){
+      outs() << "\n -------- Loop Adiecenti -------- \n";
+      //outs() << *exitBlock;
+      //outs() << *BBTopL1;
+    }
 
     /*Punto 3: si assume che ci sia solo un successore, ovvero un solo
     exitBlock*/
 
-    checkLoopControlFlowEquivalenti(DT, PDT, BBTopL1, BBTopL0, exitBlock, cont);
+    bool isLoopControlFlowEquivalent = checkLoopControlFlowEquivalent(DT, PDT, BBTopL1, BBTopL0, exitBlock, cont);
     
+    if(isLoopControlFlowEquivalent){
+      outs() << "\n -------- Loop n°" << (cont - 1) << " e Loop n°" << cont << " sono control flow equivalenti -------- \n";
+    }
+
     /*Variabili si aggiornano solo al termine di una iterazione, 
     in modo tale da contenere le informazioni della iterazione precedente*/
 
     BBTopL0 = topLoopBB(loop, exitBlock);
-    //exitingBlock = loop.getExitingBlock();
     exitBlock = loop.getExitBlock();
+    //exitingBlock = loop.getExitingBlock();
     
     cont++;
 
     //exitingBlocks.clear();
     //loop.getExitingBlocks(exitingBlocks);
-    
-
   }
   
   /*for (auto Fiter = M.begin(); Fiter != M.end(); ++Fiter)
